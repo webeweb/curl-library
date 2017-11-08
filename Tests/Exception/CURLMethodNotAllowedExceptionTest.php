ff43811f56aa1ff87978cbbf023bd56d987a8f2d
webeweb <support@webeweb.fr>
2017-11-08 14:09:29 +0100

    PHP doc

--- Tests/Exception/CURLMethodNotAllowedExceptionTest.php
+++ Tests/Exception/CURLMethodNotAllowedExceptionTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the WBWCURLLibrary package.
+ * This file is part of the curl-library package.
  *
  * (c) 2017 NdC/WBW
  *
--- Tests/Request/CURLPatchRequestTest.php
+++ Tests/Request/CURLPatchRequestTest.php
@@ -2,7 +2,7 @@
 
 /*
   /*
- * This file is part of the WBWCURLLibrary package.
+ * This file is part of the curl-library package.
  *
  * (c) 2017 NdC/WBW
  *
