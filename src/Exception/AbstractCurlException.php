ab50f9ada0bdc328fca82a6f9b2997fee6f4c908
webeweb <support@webeweb.fr>
2021-07-17 05:30:42 +0200

    Rename abstract cURL exception

--- src/Exception/AbstractCurlException.php
+++ src/Exception/AbstractCurlException.php
@@ -1,15 +1,15 @@
 <?php
 
-/**
+/*
  * This file is part of the curl-library package.
  *
- * (c) 2017 WEBEWEB
+ * (c) 2018 WEBEWEB
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace WBW\Library\CURL\Exception;
+namespace WBW\Library\Curl\Exception;
 
 use Exception;
 
@@ -17,20 +17,19 @@ use Exception;
  * Abstract cURL exception.
  *
  * @author webeweb <https://github.com/webeweb/>
- * @package WBW\Library\CURL\Exception
+ * @package WBW\Library\Curl\Exception
  * @abstract
  */
-abstract class AbstractCURLException extends Exception {
+abstract class AbstractCurlException extends Exception {
 
     /**
      * Constructor.
      *
      * @param string $message The message.
      * @param int $code The code.
-     * @param Exception $previous The previous exception.
+     * @param Exception|null $previous The previous exception.
      */
-    public function __construct($message, $code = 500, Exception $previous = null) {
+    public function __construct(string $message, int $code = 500, Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
-
 }
